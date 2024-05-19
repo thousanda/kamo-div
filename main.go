b145c8aab10abbcf74915b0a1dcada7092f44d0b
thousanda <k0k1senda@gmail.com>
2024-05-19 15:26:27 +0900

    stop infinite loop and simply print one message

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	waitTime := 1 * time.Second
-	for {
-		fmt.Printf("Hello, wait %v\n", waitTime)
-		time.Sleep(waitTime)
-		waitTime *= 2 // increase exponentially
-	}
+	fmt.Printf("Hello, eveyone\n")
 }
