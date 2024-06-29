352534b0d4876724b1a03badeca02c3bcc6852c6
thousanda <k0k1senda@gmail.com>
2024-06-29 22:48:06 +0900

    add a fake messenger which always fails

--- internal/internal/module/messaging/fake_messenger.go
+++ internal/internal/module/messaging/fake_messenger.go
@@ -15,3 +15,14 @@ func (n *nothingDoer) Broadcast() error {
 	fmt.Println("Just Broadcast!! (うそです)")
 	return nil
 }
+
+type failure struct {
+}
+
+func NewFailMessenger() Messenger {
+	return &failure{}
+}
+
+func (f *failure) Broadcast() error {
+	return fmt.Errorf("sorry, I can't broadcast")
+}
