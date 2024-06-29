63370b7dac208a464eb8d32155fedc8b169a3e17
thousanda <k0k1senda@gmail.com>
2024-06-29 22:54:03 +0900

    define attributes of the use case

--- internal/usecase/broadcast/broadcast_use_case.go
+++ internal/usecase/broadcast/broadcast_use_case.go
@@ -1,10 +1,28 @@
 package broadcast
 
+import (
+	"time"
+
+	"github.com/thousanda/kamo-div/internal/internal/module/messaging"
+	"github.com/thousanda/kamo-div/internal/internal/module/reminder"
+)
+
 type UseCase struct {
+	now                   time.Time
+	reminderMessageWriter reminder.MessageWriter
+	messenger             messaging.Messenger
 }
 
-func NewUseCase() *UseCase {
-	return &UseCase{}
+func NewUseCase(
+	now time.Time,
+	w reminder.MessageWriter,
+	m messaging.Messenger,
+) *UseCase {
+	return &UseCase{
+		now:                   now,
+		reminderMessageWriter: w,
+		messenger:             m,
+	}
 }
 
 func (u *UseCase) ScheduledBroadcast() error {
